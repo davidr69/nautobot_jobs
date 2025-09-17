dc7b5754b4b1a9bf38b035ac06a49c3fb90cb747
David Rosario <david@lavacro.com>
2025-09-16 23:19:44 -0400

    updates

--- jobs/data_quality.py
+++ jobs/data_quality.py
@@ -42,6 +42,84 @@ class VerifyPlatform(Job):
 					extra={"object": device},
 				)
 
+class VerifySerialNumber(Job):
+
+	location_to_check = ObjectVar(
+		model=Location,
+		query_params={
+			"has_devices": True,
+		}
+	)
+
+	class Meta:
+		name = "Check Serial Numbers"
+		has_sensitive_variables = False
+		description = "Check serial numbers exist for devices in the selected location"
+
+	def run(self, location_to_check):
+		device_query = Device.objects.filter(location=location_to_check)
+
+		for device in device_query:
+			self.logger.info(
+				"Checking the device %s for a serial number.",
+				device.name,
+				extra={"object": device},
+			)
+			if device.serial == "":
+				self.logger.error(
+					"Device %s does not have serial number defined.",
+					device.name,
+					extra={"object": device},
+				)
+			else:
+				self.logger.debug(
+					"Device %s has serial number: %s",
+					device.name,
+					device.serial,
+					extra={"object": device},
+				)
+
+
+class VerifyPrimaryIP(Job):
+
+	location_to_check = ObjectVar(
+		model=Location,
+		query_params={
+			"has_devices": True,
+		}
+	)
+
+	class Meta:
+		name = "Verify Device has at selected location has Primary IP configured"
+		has_sensitive_variables = False
+		description = "Check Device at selected location Primary IP configured"
+
+	def run(self, location_to_check):
+		device_query = Device.objects.filter(location=location_to_check)
+
+		for device in device_query:
+			self.logger.info(
+				"Checking the device %s for Primary IP.",
+				device.name,
+				extra={"object": device},
+			)
+
+			# Verify that the device has a primary IP
+			if device.primary_ip is None:
+				self.logger.fatal(f"{device} does not have a primary IP address configured.")
+				return
+
+			else:
+				self.logger.debug(
+					"Device %s has primary IP: %s",
+					device.name,
+					device.primary_ip,
+					extra={"object": device},
+				)
+
+
 register_jobs(
+	VerifySerialNumber,
+	VerifyPrimaryIP,
 	VerifyPlatform
 )
