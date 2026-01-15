f547c51e5cfd8fc27f0cdf21b3b0d7021188741f
David Rosario <david.rosario69@gmail.com>
2026-01-14 19:28:51 -0500

    reformatted with black

--- jobs/hello_world.py
+++ jobs/hello_world.py
@@ -1,70 +1,69 @@
-from nautobot.apps.jobs import Job, StringVar, register_jobs, JobButtonReceiver, MultiChoiceVar, ObjectVar, TextVar, IntegerVar
+from nautobot.apps.jobs import (
+    Job,
+    StringVar,
+    register_jobs,
+    JobButtonReceiver,
+    MultiChoiceVar,
+    ObjectVar,
+    TextVar,
+    IntegerVar,
+)
 from nautobot.dcim.models.devices import DeviceType
 
-name = 'Examples'       # grouping in UI
+name = "Examples"  # grouping in UI
 
 # def get_device_types():
 # 	devices = DeviceType.objects.all()
 # 	return [(str(device.pk), str(device.manufacturer)) for device in devices]
 
+
 class HelloWorldJobs(Job):
-	class Meta:
-		name = 'Hello World'    # job name
-		description = 'Update this description'
-		has_sensitive_variables = False
+    class Meta:
+        name = "Hello World"  # job name
+        description = "Update this description"
+        has_sensitive_variables = False
 
-	who = StringVar(
-		description = 'Identify yourself!',
-		default = 'hola!'
-	)
+    who = StringVar(description="Identify yourself!", default="hola!")
 
-	age = IntegerVar(
-		description = 'Your age',
-		default = 30
-	)
+    age = IntegerVar(description="Your age", default=30)
 
-	comment = TextVar(
-		description = 'Any comments?',
-		default = 'No comments'
-	)
+    comment = TextVar(description="Any comments?", default="No comments")
 
-	# devices = MultiChoiceVar(
-	# 	description = 'Select device type(s)',
-	# 	choices = (get_device_types())
-	# )
-	devices = ObjectVar(
-		model = DeviceType,
-		description = 'Select device type(s)'
-	)
+    # devices = MultiChoiceVar(
+    # 	description = 'Select device type(s)',
+    # 	choices = (get_device_types())
+    # )
+    devices = ObjectVar(model=DeviceType, description="Select device type(s)")
 
-	def run(self, *, who, age, comment, devices):
-		self.logger.info('Hello, %s! You are %s years old.', who, age)
-		if comment:
-			self.logger.info('Comment: %s', comment)
-		if devices:
-			self.logger.info('Selected device type IDs: %s', devices)
+    def run(self, *, who, age, comment, devices):
+        self.logger.info("Hello, %s! You are %s years old.", who, age)
+        if comment:
+            self.logger.info("Comment: %s", comment)
+        if devices:
+            self.logger.info("Selected device type IDs: %s", devices)
 
 
 class HelloWorldButtonReceiver(JobButtonReceiver):
-	class Meta:
-		name = 'Hello World Button Receiver'
-		description = 'A Job Button Receiver example'
+    class Meta:
+        name = "Hello World Button Receiver"
+        description = "A Job Button Receiver example"
 
-	def run(self, context):
-		self.log_info("Hello from the Job Button Receiver!")
-		self.log_info(f"Context: {context}")
+    def run(self, context):
+        self.log_info("Hello from the Job Button Receiver!")
+        self.log_info(f"Context: {context}")
 
-
-...
 
 class HelloJobsWithApproval(Job):
 
-	class Meta:
-		name = "Hello World with Approval Required"
-		approval_required = True
-		has_sensitive_variables = False
+    class Meta:
+        name = "Hello World with Approval Required"
+        approval_required = True
+        has_sensitive_variables = False
 
-	def run(self):
-		self.logger.debug("Hello, this is my first Nautobot Job that requires approval.")
+    def run(self):
+        self.logger.debug(
+            "Hello, this is my first Nautobot Job that requires approval."
+        )
+
 
 register_jobs(HelloWorldJobs, HelloWorldButtonReceiver, HelloJobsWithApproval)
--- jobs/hello_world_job_button.py
+++ jobs/hello_world_job_button.py
@@ -3,15 +3,23 @@ from nautobot.apps.jobs import Job, register_jobs, JobButtonReceiver
 
 name = "Job Button Receivers"
 
+
 class HelloWorldJobButton(JobButtonReceiver):
 
-	class Meta:
-		name = "This is my first JobButton Receiver"
+    class Meta:
+        name = "This is my first JobButton Receiver"
 
-	def receive_job_button(self, obj):
-		self.logger.info("This is my first Nautobot Job Button.", extra={"object": obj})
-		self.logger.info("This is my first Nautobot Job Button.", extra={"object": obj.name})
-		self.logger.info("This is my first Nautobot Job Button.", extra={"object": obj.status})
-		self.logger.info("This is my first Nautobot Job Button.", extra={"object": obj.role})
+    def receive_job_button(self, obj):
+        self.logger.info("This is my first Nautobot Job Button.", extra={"object": obj})
+        self.logger.info(
+            "This is my first Nautobot Job Button.", extra={"object": obj.name}
+        )
+        self.logger.info(
+            "This is my first Nautobot Job Button.", extra={"object": obj.status}
+        )
+        self.logger.info(
+            "This is my first Nautobot Job Button.", extra={"object": obj.role}
+        )
+
 
 register_jobs(HelloWorldJobButton)
