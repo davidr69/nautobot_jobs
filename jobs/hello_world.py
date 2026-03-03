e83b97e2553f0df3428a03fd74947ff443e076aa
David Rosario <david@lavacro.com>
2026-03-03 12:53:44 -0500

    time limit test

--- jobs/hello_world.py
+++ jobs/hello_world.py
@@ -18,6 +18,8 @@ class HelloWorldJobs(Job):
         name = "Hello World"  # job name
         description = "Update this description"
         has_sensitive_variables = False
+        soft_time_limit = 300
+        time_limit = 600
 
     who = StringVar(description="Identify yourself!", default="hola!")
 
