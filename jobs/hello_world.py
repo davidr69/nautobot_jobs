c79cad149a223ab4f6f402d35b555ca1557b49ff
David Rosario <david.rosario69@gmail.com>
2026-03-03 19:11:35 -0500

    time limit test

--- jobs/hello_world.py
+++ jobs/hello_world.py
@@ -18,7 +18,7 @@ class HelloWorldJob(Job):
         name = "Hello World"  # job name
         description = "It's what you would expect!"
         has_sensitive_variables = False
-        soft_time_limit = 900
+        soft_time_limit = 1000
         time_limit = 1300
         read_only = True
         is_singleton = True
