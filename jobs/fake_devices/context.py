37af0e18beab468df8b4f2ad0e80dfb168304430
David Rosario <david@lavacro.com>
2026-03-24 10:26:36 -0400

    fixed argument list

--- jobs/fake_devices/context.py
+++ jobs/fake_devices/context.py
@@ -2,10 +2,11 @@ import netaddr
 from nautobot_design_builder.context import Context
 
 class MyDesignContext(Context):
-    def __init__(self, job, **kwargs):
-        super().__init__(job, **kwargs)
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
         # Calculate IPs here so the YAML stays clean
-        prefix = netaddr.IPNetwork(self.mgmt_prefix)
+        mgmt_prefix = getattr(self, "mgmt_prefix", kwargs.get("mgmt_prefix"))
+        prefix = netaddr.IPNetwork(mgmt_prefix)
         self.device_ips = {
             "device1": str(prefix[1]) + f"/{prefix.prefixlen}",
             "device2": str(prefix[2]) + f"/{prefix.prefixlen}",
