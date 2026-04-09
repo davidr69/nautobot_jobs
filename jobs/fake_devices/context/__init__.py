7c950d049c0909cdfb70e2bc38d918686094caae
David Rosario <david.rosario69@gmail.com>
2026-04-09 00:29:56 -0400

    use yaml instead of json

--- jobs/fake_devices/context/__init__.py
+++ jobs/fake_devices/context/__init__.py
@@ -1,5 +1,5 @@
-import json
 from pathlib import Path
+import yaml
 from nautobot_design_builder.context import Context
 
 _DATA_FILE = Path(__file__).parent.parent / "data" / "fake_devices.yml"
@@ -9,4 +9,4 @@ class MyDesignContext(Context):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         with open(_DATA_FILE, encoding="utf-8") as f:
-            self.device_ips = json.load(f)
+            self.data = yaml.safe_load(f)
