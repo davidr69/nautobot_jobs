ef2cd8bb93a41dad791b4596c2ca91a1f1400c76
David Rosario <david.rosario69@gmail.com>
2026-04-09 22:49:59 -0400

    update design

--- jobs/fake_devices/design_job.py
+++ jobs/fake_devices/design_job.py
@@ -1,6 +1,5 @@
-from nautobot.apps.jobs import StringVar, register_jobs  # , ObjectVar
-
-# from nautobot.dcim.models import Location
+from nautobot.apps.jobs import StringVar, register_jobs
+from nautobot.dcim.models import Device
 from nautobot_design_builder.design_job import DesignJob
 from .context import MyDesignContext
 
@@ -14,6 +13,14 @@ class MyDeviceDesign(DesignJob):
         name = "Deploy Home Devices"
         design_file = "templates/design.yml.j2"
         context_class = MyDesignContext
+
+    def post_implementation(self, context, design_builder):
+        for device_data in context.data["devices"]:
+            device = Device.objects.get(name=device_data["hostname"])
+            ip = device.interfaces.get(name="GigabitEthernet0/0").ip_addresses.first()
+            if ip:
+                device.primary_ip4 = ip
+                device.save()
 
 
 register_jobs(MyDeviceDesign)
