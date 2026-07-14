33febaeb14dd29caa6f8a866968b7d2c5cb9943c
David Rosario <david.rosario69@gmail.com>
2026-07-13 23:01:51 -0400

    retry logic

--- jobs/hello_world.py
+++ jobs/hello_world.py
@@ -1,3 +1,5 @@
+import random
+
 from nautobot.apps.jobs import (
     Job,
     StringVar,
@@ -10,6 +12,8 @@ from nautobot.apps.jobs import (
 )
 from nautobot.dcim.models.devices import DeviceType
 from django_redis import get_redis_connection
+
+from celery import current_app, current_task
 
 name = "Examples"
 
@@ -36,12 +40,18 @@ class HelloWorldJob(Job):
     # )
     devices = ObjectVar(model=DeviceType, description="Select device type(s)")
 
-    def run(self, *args, who, age, comment, devices):
+    def run(self, *, who, age, comment, devices):
         self.logger.info("Hello, %s! You are %s years old.", who, age)
         redis_client = get_redis_connection("default")
         job_name = self.__class__.__name__
         countdown = redis_client.get(f"{job_name}.countdown")
         self.logger.info(f"countdown? {countdown}")
+
+        val = random.random()
+        self.logger.info(f"{val=}")
+
+        if val < 0.5:
+            raise SystemError("Fake error")
 
         if comment:
             self.logger.info("Comment: %s", comment)
@@ -51,6 +61,36 @@ class HelloWorldJob(Job):
     def on_failure(self, exc, task_id, args, kwargs, einfo):
         self.logger.error("Job failed!")
         self.logger.error(f"{exc=}, {task_id=}, {args=}, {kwargs=}, {einfo=}")
+
+        redis_client = get_redis_connection("default")
+        job_name = self.__class__.__name__
+        countdown = redis_client.get(f"{job_name}.countdown")
+
+        if countdown:
+            try:
+                num = int(countdown)
+                if num == 0:
+                    redis_client.delete(f"{job_name}.countdown")
+                    self.logger.error("Retries exhausted")
+                    return
+
+                num -= 1
+                redis_client.set(f"{job_name}.countdown", 600, num)
+
+                task_name = getattr(current_task, "name", None) or getattr(self, "task_name", None)
+                if not task_name:
+                    self.logger.error("Unable to determine Celery task name; cannot reschedule automatically.")
+                    return
+
+                args = args or []
+                kwargs = kwargs or {}
+
+                current_app.send_task(task_name, args=args, kwargs=kwargs, countdown=300)
+                self.log.info("Scheduled job")
+            except ValueError:
+                self.logger.error("Invalid countdown number!")
+        else:
+            redis_client.set(f"{job_name}.countdown", 600, 0)
 
 
 class Noop(Job):
